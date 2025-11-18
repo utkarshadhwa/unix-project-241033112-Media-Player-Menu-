48bbecb4f583f1004c408205ee7db302d3b8e3c5
utkarshadhwa <241033112@juitsolan.in>
2025-11-18 22:13:40 +0530

    Add files via upload

---
+++ media_player.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+while true; do
+    echo "Available media files:"
+    files=(*.mp3 *.mp4)
+    i=1
+    for file in "${files[@]}"; do
+        echo "$i) $file"
+        ((i++))
+    done
+
+    read -p "Enter the number of the file you want to play (or 0 to exit): " choice
+
+    if [[ $choice -eq 0 ]]; then
+        echo "Exiting..."
+        break
+    elif [[ $choice -ge 1 && $choice -le ${#files[@]} ]]; then
+        selected_file=${files[$choice-1]}
+        echo "Now playing: $selected_file"  # simulate playback
+        sleep 2  # wait 2 seconds to simulate playing
+    else
+        echo "Invalid choice. Try again."
+    fi
+
+    echo ""
+done
+
